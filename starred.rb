93de7a08d8ee695bce892d355924b792534ebf35
Philippe Mongeau <ph.mongeau@gmail.com>
2010-02-08 22:01:49 -0500

    removed number before links for better bookmarks titles

--- starred.rb
+++ starred.rb
@@ -1,40 +1,31 @@
 require 'rss'
 require 'open-uri'
 
-links = ""
-refine = ""
-linksH = {}
-linksA = []
+links = "" #raw link
+linksH = [] #title and link for html
+
+#open the rss and get the items
 open( "http://www.google.com/reader/public/atom/user/13310254936429827125/state/com.google/starred?n=1000") do |http|
 	response = http.read
 	result = RSS::Parser.parse(response, false)
 	items = result.items
 	
-	i = 0
 	items.each do |item|
-		links << item.link.href.to_s
-		links << "\n"
-		linksH[i.to_s << " - " << item.title.to_s.gsub('<title type="html">', '').gsub('</title>', '')] = item.link.href.to_s
-		linksA << [i.to_s << " - " << item.title.to_s.gsub('<title type="html">', '').gsub('</title>', ''), item.link.href.to_s]
-		i += 1
+		links << item.link.href.to_s << "\n"
+		linksH << [item.title.to_s.gsub('<title type="html">', '').gsub('</title>', ''), item.link.href.to_s]
 	end
 end
 
-puts links.length
-puts linksH.length
 
+#writing the raw links file
 File::open( 'starred.txt', 'w') do |f|
 	f << links
-	#puts "write to file"
 end
 
+#writing the html file
 File::open( 'starred.html', 'w') do |f|
 	f << "<html>\n<head>\n<title>Starred Items</title>\n</head>\n<body>\n"
-#	linksH.each do |title, link|
-#		f << "<a href='#{link}'>#{title}</a>"
-#		f << "<br> \n"
-#	end
-	linksA.each do |item|
+	linksH.each do |item|
 		f << "<a href='#{item[1]}'>#{item[0]}</a><br>\n"
 	end
 	f << "</body>\n</html>"
