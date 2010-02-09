305071be5aef05c29067c71b4f1c06e685fb7090
Philippe Mongeau <ph.mongeau@gmail.com>
2010-02-08 22:33:00 -0500

    Adapted to a publishable form and added a README

--- supernova.rb
+++ supernova.rb
@@ -5,7 +5,7 @@ links = "" #raw link
 linksH = [] #title and link for html
 
 #open the rss and get the items
-open( "http://www.google.com/reader/public/atom/user/13310254936429827125/state/com.google/starred?n=1000") do |http|
+open( "http://www.google.com/reader/public/atom/user/@@@@@@@@@@@@@@/state/com.google/starred?n=1000") do |http|
 	response = http.read
 	result = RSS::Parser.parse(response, false)
 	items = result.items
@@ -18,12 +18,12 @@ end
 
 
 #writing the raw links file
-File::open( 'starred.txt', 'w') do |f|
+File::open( 'stars.txt', 'w') do |f|
 	f << links
 end
 
 #writing the html file
-File::open( 'starred.html', 'w') do |f|
+File::open( 'stars.html', 'w') do |f|
 	f << "<html>\n<head>\n<title>Starred Items</title>\n</head>\n<body>\n"
 	linksH.each do |item|
 		f << "<a href='#{item[1]}'>#{item[0]}</a><br>\n"
