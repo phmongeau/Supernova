5dbc9a725d30ab64d369ebb107ee7e43e5d5a908
Philippe Mongeau <ph.mongeau@gmail.com>
2010-02-08 21:36:23 -0500

    html alomost working, need to change the sorting of the hash

--- starred.rb
+++ starred.rb
@@ -3,20 +3,35 @@ require 'open-uri'
 
 links = ""
 refine = ""
+linksH = {}
 
 open( "http://www.google.com/reader/public/atom/user/13310254936429827125/state/com.google/starred?n=1000") do |http|
 	response = http.read
 	result = RSS::Parser.parse(response, false)
 	items = result.items
 	
+	i = 0
 	items.each do |item|
-		puts item.link.href
 		links << item.link.href.to_s
 		links << "\n"
+		linksH[i.to_s << " - " << item.title.to_s.gsub('<title type="html">', '').gsub('</title>', '')] = item.link.href.to_s
+		i += 1
 	end
 end
 
+puts links.length
+puts linksH.length
+
 File::open( 'starred.txt', 'w') do |f|
 	f << links
-	puts "write to file"
+	#puts "write to file"
+end
+
+File::open( 'starred.html', 'w') do |f|
+	f << "<html>\n<head>\n<title>Starred Items</title>\n</head>\n<body>\n"
+	linksH.each do |title, link|
+		f << "<a href='#{link}'>#{title}</a>"
+		f << "<br> \n"
+	end
+	f << "</body>\n</html>"
 end
